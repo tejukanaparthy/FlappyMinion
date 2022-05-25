991325cac3c2e58e553653ddfc8a9c1be5db968b
tristawa <106182268+tristawa@users.noreply.github.com>
2022-05-25 12:11:31 -0700

    Add files via upload

--- BottomPipe.java
+++ BottomPipe.java
@@ -9,18 +9,13 @@ public class BottomPipe {
     private Image bottomPipe;
     private int xLoc = 0, yLoc = 0;
     
-    /**
-     * Default constructor
-     */
     public BottomPipe(int initialWidth, int initialHeight) {
         bottomPipe = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("flappy minion graphics/bottompipe.png"));
         scaleBottomPipe(initialWidth, initialHeight);
     }
     
     /**
-     * Method to scale the BottomPipe sprite into the desired dimensions
-     * @param width The desired width of the BottomPipe
-     * @param height The desired height of the BottomPipe
+     * method to scale the BottomPipe sprite into the desired dimensions
      */
     public void scaleBottomPipe(int width, int height) {
         bottomPipe = bottomPipe.getScaledInstance(width, height, Image.SCALE_SMOOTH);        
@@ -34,50 +29,26 @@ public class BottomPipe {
         return bottomPipe;
     }
     
-    /**
-     * Method to obtain the width of the BottomPipe object
-     * @return int
-     */
     public int getWidth() {
         return bottomPipe.getWidth(null);
     }
-    
-    /**
-     * Method to obtain the height of the BottomPipe object
-     * @return int
-     */
+ 
     public int getHeight() {
         return bottomPipe.getHeight(null);
     }
     
-    /**
-     * Method to set the x location of the BottomPipe object
-     * @param x
-     */
     public void setX(int x) {
         xLoc = x;
     }
     
-    /**
-     * Method to get the x location of the BottomPipe object
-     * @return int
-     */
     public int getX() {
         return xLoc;
     }
     
-    /**
-     * Method to set the y location of the BottomPipe object
-     * @param y
-     */
     public void setY(int y) {
         yLoc = y;
     }
-    
-    /**
-     * Method to get the y location of the BottomPipe object
-     * @return int
-     */
+
     public int getY() {
         return yLoc;
     }
--- TopPipe.java
+++ TopPipe.java
@@ -9,9 +9,6 @@ public class TopPipe {
     private Image topPipe;
     private int xLoc = 0, yLoc = 0;
 
-    /**
-     * Default constructor
-     */
     public TopPipe(int initialWidth, int initialHeight) {
         topPipe = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("flappy minion graphics/toppipe.png"));
         scaleTopPipe(initialWidth, initialHeight);
@@ -19,8 +16,6 @@ public class TopPipe {
 
     /**
      * Method to scale the topPipe sprite into the desired dimensions
-     * @param width The desired width of the topPipe
-     * @param height The desired height of the topPipe
      */
     public void scaleTopPipe(int width, int height) {
         topPipe = topPipe.getScaledInstance(width, height, Image.SCALE_SMOOTH);        
@@ -34,50 +29,26 @@ public class TopPipe {
         return topPipe;
     }
 
-    /**
-     * Method to obtain the width of the TopPipe object
-     * @return int
-     */
     public int getWidth() {
         return topPipe.getWidth(null);
     }
 
-    /**
-     * Method to obtain the height of the TopPipe object
-     * @return int
-     */
     public int getHeight() {
         return topPipe.getHeight(null);
     }
 
-    /**
-     * Method to set the x location of the TopPipe object
-     * @param x
-     */
     public void setX(int x) {
         xLoc = x;
     }
 
-    /**
-     * Method to get the x location of the TopPipe object
-     * @return int
-     */
     public int getX() {
         return xLoc;
     }
 
-    /**
-     * Method to set the y location of the TopPipe object
-     * @param y
-     */
     public void setY(int y) {
         yLoc = y;
     }
 
-    /**
-     * Method to get the y location of the TopPipe object
-     * @return int
-     */
     public int getY() {
         return yLoc;
     }
